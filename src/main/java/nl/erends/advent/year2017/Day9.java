02dde05a127dd06397c1b1adeae74cc96cfb110f
MErends <mark.erends@gmail.com>
2017-12-09 09:51:52 +0100

    2017 Day 9

---
+++ src/main/java/nl/erends/advent/year2017/Day9.java
@@ -0,0 +1,41 @@
+package nl.erends.advent.year2017;
+
+import nl.erends.advent.util.FileIO;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        char[] tokens = FileIO.getFileAsString("2017day9.txt").toCharArray();
+        int depth = 0;
+        int score = 0;
+        int cancelled = 0;
+        boolean inGarbage = false;
+        for (int index = 0; index < tokens.length; index++) {
+            char token = tokens[index];
+            if (inGarbage && token != '>' && token != '!') {
+                cancelled++;
+                continue;
+            }
+            switch (token) {
+                case '{':
+                    depth++;
+                    break;
+                case '}':
+                    score += depth;
+                    depth--;
+                    break;
+                case '!':
+                    index++;
+                    break;
+                case '<':
+                    inGarbage = true;
+                    break;
+                case '>':
+                    inGarbage = false;
+                    break;
+            }
+        }
+        System.out.println(score);
+        System.out.println(cancelled);
+    }
+}
