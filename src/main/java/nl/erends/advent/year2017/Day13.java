130749491d5513d5d43fa7ad22a0badc43badff4
Mark Erends <15726799+MErends@users.noreply.github.com>
2017-12-13 15:57:59 +0100

    2017 Day 13

--- src/main/java/nl/erends/advent/year2017/Day13.java
+++ src/main/java/nl/erends/advent/year2017/Day13.java
@@ -7,54 +7,84 @@ import java.util.List;
 public class Day13 {
 
 	private static int[] depthAtLevel;
+	private static int[] scannerposition;
+	private static boolean[] scannerMovingDown;
 
 	public static void main(String[] args) {
 		List<String> lines = FileIO.getFileAsList("2017day13.txt");
 		depthAtLevel = new int[Integer.parseInt(lines.get(lines.size() - 1).split(":")[0].trim()) + 1];
+		scannerposition = new int[Integer.parseInt(lines.get(lines.size() - 1).split(":")[0].trim()) + 1];
+		scannerMovingDown = new boolean[Integer.parseInt(lines.get(lines.size() - 1).split(":")[0].trim()) + 1];
 		for (String line : lines) {
 			String[] words = line.split(":");
 			depthAtLevel[Integer.parseInt(words[0].trim())] = Integer.parseInt(words[1].trim());
+			scannerposition[Integer.parseInt(words[0].trim())] = Integer.parseInt(words[1].trim()) != 0 ? 1 : -1;
+			scannerMovingDown[Integer.parseInt(words[0].trim())] = true;
 		}
-		System.out.println(getPenalty(0));
+		initializeScannerPosition();
+		System.out.println(getPenalty());
 		int delay = 0;
-		while (true) {
-			int penalty = getPenalty(delay);
-//			System.out.println("Delay: " + delay + ", Penalty: " + penalty);
-			if (penalty == 0) break;
+		while (hasPenalty()) {
 			delay++;
+			updateScanners();
 		}
 		System.out.println(delay);
 	}
 
-	private static int getPenalty(int delay) {
+	private static int getPenalty() {
 		int penalty = 0;
-		for (int index = 0; index < depthAtLevel.length; index++) {
-			int depth = depthAtLevel[index];
-			int scannerPosition = getScannerPosition(index, delay + index);
-			if (scannerPosition == 1) {
-				penalty += index * depth;
+		for (int level = 0; level < depthAtLevel.length; level++) {
+			int depth = depthAtLevel[level];
+			if (scannerposition[level] == 1) {
+				penalty += level * depth;
 			}
 		}
 		return penalty;
 	}
 
-	private static int getScannerPosition(int level, int atSecond) {
-		int depth = depthAtLevel[level];
-		if (depth == 0) return -1;
-		int position = 1;
-		boolean goingDown = true;
-		for (int second = 0; second < atSecond; second++) {
-			if (goingDown) {
-				position++;
-				if (position == depth) goingDown = false;
-			} else {
-				position--;
-				if (position == 1) goingDown = true;
+	private static boolean hasPenalty() {
+		for (int level = 0; level < depthAtLevel.length; level++) {
+			if (scannerposition[level] == 1) {
+				return true;
 			}
 		}
-		return position;
+		return false;
 	}
 
+	private static void updateScanners() {
+		for (int level = 0; level < depthAtLevel.length; level++) {
+			int depth = depthAtLevel[level];
+			if (depth == 0)
+				continue;
+			int position = scannerposition[level];
+			if (scannerMovingDown[level]) {
+				position++;
+				if (position == depth)
+					scannerMovingDown[level] = false;
+			} else {
+				position--;
+				if (position == 1)
+					scannerMovingDown[level] = true;
+			}
+			scannerposition[level] = position;
+		}
+	}
 
-
+	private static void initializeScannerPosition() {
+		for (int level = 0; level < depthAtLevel.length; level++) {
+			int depth = depthAtLevel[level];
+			if (depth == 0) continue;
+			int position = 1;
+			for (int i = 0; i < level; i++) {
+				if (scannerMovingDown[level]) {
+					position++;
+					if (position == depth) scannerMovingDown[level] = false;
+				} else {
+					position--;
+					if (position == 1) scannerMovingDown[level] = true;
+				}
+			}
+			scannerposition[level] = position;
+		}
+	}
 }
