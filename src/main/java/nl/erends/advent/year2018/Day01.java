f23f83cb63467061a295057273392277830b27cd
MErends <mark.erends@gmail.com>
2018-12-01 12:24:27 +0100

    Refactor 2018 Day 01

--- src/main/java/nl/erends/advent/year2018/Day01.java
+++ src/main/java/nl/erends/advent/year2018/Day01.java
@@ -5,27 +5,25 @@ import nl.erends.advent.util.FileIO;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Day01 {
 
     public static void main(String[] args) {
         List<String> input = FileIO.getFileAsList("2018day01.txt");
         long start = System.currentTimeMillis();
-        System.out.println(input.stream().mapToInt(Integer::parseInt).sum());
+        List<Integer> integerInput = input.stream().map(Integer::parseInt).collect(Collectors.toList());
+        System.out.println(integerInput.stream().mapToInt(i -> i).sum());
         long mid = System.currentTimeMillis();
         Set<Integer> freqsSeen = new HashSet<>();
         int freq = 0;
         freqsSeen.add(freq);
-        for (int index = 0; ; index++) {
-            int change = Integer.parseInt(input.get(index % input.size()));
-            freq += change;
-            if (!freqsSeen.contains(freq)) {
-                freqsSeen.add(freq);
-            } else {
-                System.out.println(freq);
-                break;
-            }
-        }
+        int index = 0;
+        do {
+            freq += integerInput.get(index % integerInput.size());
+            index++;
+        } while (freqsSeen.add(freq));
+        System.out.println(freq);
         long end = System.currentTimeMillis();
         System.out.println("Part 1: " + (mid - start) + " millis.\nPart 2: " + (end - mid) + " millis.");
     }
