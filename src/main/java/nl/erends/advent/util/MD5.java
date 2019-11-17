2a9be82b10d7b6057a471affdc5e7aeb5de9ba1f
MErends <mark.erends@gmail.com>
2019-11-17 13:26:03 +0100

    Cleanup MD5

--- src/main/java/nl/erends/advent/util/MD5.java
+++ src/main/java/nl/erends/advent/util/MD5.java
@@ -1,28 +1,26 @@
 package nl.erends.advent.util;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5 {
-
+    
     private static MessageDigest md5er;
     static {
         try {
             md5er = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            throw new IllegalStateException(e);
         }
+    }
+    
+    private MD5() {
     }
 
     public static String getHash(String input) {
         StringBuilder sb = new StringBuilder();
-        byte[] digest = new byte[0];
-        try {
-            digest = md5er.digest(input.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        byte[] digest = md5er.digest(input.getBytes(StandardCharsets.UTF_8));
         for (byte b : digest) {
             sb.append(String.format("%02x", b & 0xff));
         }
