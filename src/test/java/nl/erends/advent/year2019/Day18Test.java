3379488da6c9305e0b0e1485ef4491b11496df50
MErends <mark.erends@gmail.com>
2020-01-01 21:36:58 +0100

    2019 Day 18 not solved

---
+++ src/test/java/nl/erends/advent/year2019/Day18Test.java
@@ -0,0 +1,16 @@
+package nl.erends.advent.year2019;
+
+import nl.erends.advent.util.Util;
+import org.junit.Test;
+
+import static nl.erends.advent.util.Assert.assertEquals;
+
+public class Day18Test {
+
+    @Test
+    public void day18Test1() {
+        Day18 problem = new Day18();
+        problem.setInput(Util.readInput(2019, 18, 1));
+        assertEquals(136, problem.solve1());
+    }
+}
