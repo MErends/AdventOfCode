7e65fc664bc12ea7db0be03bc0174a74b7d20c2c
MErends <mark.erends@gmail.com>
2020-12-09 14:03:55 +0100

    2020 Day 09

---
+++ src/test/java/nl/erends/advent/year2020/Day09Test.java
@@ -0,0 +1,18 @@
+package nl.erends.advent.year2020;
+
+import nl.erends.advent.util.Util;
+import org.junit.Test;
+
+import static nl.erends.advent.util.Assert.assertEquals;
+
+public class Day09Test {
+    
+    @Test
+    public void day09Test() {
+        Day09 problem = new Day09();
+        problem.setInput(Util.readInput(2020, 9, 1));
+        problem.setPreambleSize(5);
+        assertEquals(127, problem.solve1());
+        assertEquals(62, problem.solve2());
+    }
+}
