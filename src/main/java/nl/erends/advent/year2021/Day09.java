74303536cc1d47cdf7a45aec33fda285d904fa3e
MErends <mark.erends@gmail.com>
2021-12-09 20:22:55 +0100

    2021 Day 09, clean-up

--- src/main/java/nl/erends/advent/year2021/Day09.java
+++ src/main/java/nl/erends/advent/year2021/Day09.java
@@ -4,7 +4,6 @@ import nl.erends.advent.util.AbstractProblem;
 import nl.erends.advent.util.Util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,63 +22,66 @@ public class Day09 extends AbstractProblem<List<String>, Integer> {
     }
     
     private int[][] grid;
-    private int[][] basins;
+    private int[][] basinGrid;
     
     @Override
     protected Integer solve1() {
-        List<Integer> basinList = new ArrayList<>();
+        
+        List<Integer> basins = new ArrayList<>();
         grid = new int[input.size()][input.get(0).length()];
-        basins = new int[input.size()][input.get(0).length()];
+        basinGrid = new int[input.size()][input.get(0).length()];
         for (int y = 0; y < input.size(); y++) {
             for (int x = 0; x < input.get(y).length(); x++) {
                 grid[y][x] = Integer.parseInt("" + input.get(y).charAt(x));
-                basins[y][x] = grid[y][x];
+                basinGrid[y][x] = grid[y][x];
             }
         }
+        
         int totalRisk = 0;
         for (int y = 0; y < grid.length; y++) {
             for (int x = 0; x < grid[y].length; x++) {
                  if (isMinimum(x, y)) {
                      totalRisk += grid[y][x] + 1;
-                     basinList.add(calculateBasins(x, y));
+                     basins.add(calculateBasin(x, y));
                  }
              }
          }
-        Collections.sort(basinList);
-        answer2 = basinList.stream()
-                .mapToInt(i -> i)
-                .skip(basinList.size() - 3L)
+        
+        answer2 = basins.stream()
+                .sorted()
+                .skip(basins.size() - 3L)
                 .reduce(1, (b1, b2) -> b1 * b2);
          return totalRisk;
     }
     
     private boolean isMinimum(int x, int y) {
         int value = grid[y][x];
-        if (x > 0 && grid[y][x - 1] <= value) { // left
-            return false;
-        }
-        if (x < grid[y].length - 1 && grid[y][x + 1] <= value) { // right
-            return false;
-        }
-        if (y > 0 && grid[y - 1][x] <= value) { // up
-            return false;
-        }
-        return y >= grid.length - 1 || grid[y + 1][x] > value; // down
+        return getValue(x - 1, y) > value
+                && getValue(x + 1, y) > value
+                && getValue(x, y - 1) > value
+                && getValue(x, y + 1) > value;
     }
     
-    private int calculateBasins(int x, int y) {
+    private int getValue(int x, int y) {
+        if (x < 0 || y < 0 || y == grid.length || x == grid[y].length) {
+            return 9;
+        } else {
+            return grid[y][x];
+        }
+    }
+    
+    private int calculateBasin(int x, int y) {
         if (x < 0 || y < 0 || y == grid.length || x == grid[y].length) {
             return 0;
         }
-        if (basins[y][x] == 9) {
+        if (basinGrid[y][x] == 9) {
             return 0;
         } else {
-            basins[y][x] = 9;
-            return 1 + calculateBasins(x + 1, y)
-                    + calculateBasins(x - 1, y)
-                    + calculateBasins(x, y + 1)
-                    + calculateBasins(x, y - 1);
+            basinGrid[y][x] = 9;
+            return 1 + calculateBasin(x + 1, y)
+                    + calculateBasin(x - 1, y)
+                    + calculateBasin(x, y + 1)
+                    + calculateBasin(x, y - 1);
         }
-        
     }
 }
