a2cc88aec58bed5bc53b2cf1f39d43c0f2344fc4
MErends <mark.erends@gmail.com>
2021-12-10 08:23:34 +0100

    2021 Day 10

---
+++ src/test/java/nl/erends/advent/year2021/Day10Test.java
@@ -0,0 +1,18 @@
+package nl.erends.advent.year2021;
+
+import nl.erends.advent.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class Day10Test {
+
+    @Test
+    void day10Test() {
+        Day10 problem = new Day10();
+        problem.setInput(Util.readInput(2021, 10, 1));
+        assertThat(problem.solve1(), is(26397L));
+        assertThat(problem.solve2(), is(288957L));
+    }
+}
