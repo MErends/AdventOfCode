1d15fd60ed2f0ccfc6fde62cdc3e85f3c3d7cfbd
MErends <mark.erends@gmail.com>
2022-11-24 21:42:14 +0100

    The Lost Codes: 2015 Day 23

---
+++ src/test/java/nl/erends/advent/year2015/Day23Test.java
@@ -0,0 +1,19 @@
+package nl.erends.advent.year2015;
+
+import nl.erends.advent.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+class Day23Test {
+
+    @Test
+    void day23Test() {
+        Day23 problem = new Day23();
+        problem.setInput(Util.readInput(2015, 23));
+        assertThat(problem.solve1(), is(184));
+        assertThat(problem.solve2(), is(231));
+    }
+}
