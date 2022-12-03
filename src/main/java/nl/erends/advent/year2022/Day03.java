4b37851273cc7bfc6b721480a3c99432137137bb
MErends <mark.erends@gmail.com>
2022-12-03 21:20:54 +0100

    2022 Day 3, less lines more better

--- src/main/java/nl/erends/advent/year2022/Day03.java
+++ src/main/java/nl/erends/advent/year2022/Day03.java
@@ -4,13 +4,15 @@ import nl.erends.advent.util.AbstractProblem;
 import nl.erends.advent.util.Util;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * --- Day 3: Rucksack Reorganization ---
  * <p>One Elf has the important job of loading all of the rucksacks with
  * supplies for the jungle journey. The list of items for each rucksack is given
  * as characters all on a single line. Find the item type that appears in both
- * compartments of each rucksack. What is the sum of the priorities of thoseitem types?
+ * compartments of each rucksack. What is the sum of the priorities of those
+ * item types?
  * <p><a href="https://adventofcode.com/2022/day/3">2022 Day 3</a>
  */
 public class Day03 extends AbstractProblem<List<String>, Integer> {
@@ -21,30 +23,26 @@ public class Day03 extends AbstractProblem<List<String>, Integer> {
 
     @Override
     protected Integer solve1() {
-        int priority = 0;
-        List<Character> mixedItems = input.stream().map(this::getMixedItem).toList();
-        for (char c : mixedItems) {
-            priority += c - (Character.isLowerCase(c) ? 96 : 38);
-        }
-        return priority;
+        return input.stream()
+                .map(this::getMixedItem)
+                .mapToInt(c -> c - (Character.isLowerCase(c) ? 96 : 38))
+                .sum();
     }
 
     @Override
     public Integer solve2() {
-        int priority = 0;
-        for (int i = 0; i < input.size(); i += 3) {
-            char c = getCommonChar(input.subList(i, i + 3));
-            priority += c - (Character.isLowerCase(c) ? 96 : 38);
-        }
-        return priority;
+        return IntStream.range(0, input.size() / 3)
+                .map(i -> i * 3)
+                .mapToObj(i -> getCommonChar(input.subList(i, i + 3)))
+                .mapToInt(c -> c - (Character.isLowerCase(c) ? 96 : 38))
+                .sum();
     }
 
     private char getMixedItem(String line) {
         for (int indexA = 0; indexA < line.length() / 2; indexA++) {
             for (int indexB = line.length() / 2; indexB < line.length(); indexB++) {
                 char a = line.charAt(indexA);
-                char b = line.charAt(indexB);
-                if (a == b) {
+                if (a == line.charAt(indexB)) {
                     return a;
                 }
             }
