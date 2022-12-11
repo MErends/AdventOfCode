2db790afb58501068704d317ea8f06060fe240a2
MErends <mark.erends@gmail.com>
2022-12-11 19:34:32 +0100

    2022 Day 11, fine I'll use lcm

--- src/main/java/nl/erends/advent/year2022/Day11.java
+++ src/main/java/nl/erends/advent/year2022/Day11.java
@@ -1,14 +1,12 @@
 package nl.erends.advent.year2022;
 
 import nl.erends.advent.util.AbstractProblem;
-import nl.erends.advent.util.Timer;
 import nl.erends.advent.util.Util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.function.LongUnaryOperator;
 
 /**
  * --- Day 11: Monkey in the Middle ---
@@ -23,132 +21,86 @@ import java.util.Map;
 public class Day11 extends AbstractProblem<List<String>, Long> {
 
     private final List<Monkey> monkeys = new ArrayList<>();
-    private boolean part2 = false;
-    private final List<Integer> divisors = new ArrayList<>();
+    private long supermodulo = Integer.MAX_VALUE;
+    private boolean part2;
+
 
     public static void main(String[] args) {
-        new Day11().setAndSolve(Util.readInput(2022, 11, 1));
+        new Day11().setAndSolve(Util.readInput(2022, 11));
     }
 
     @Override
     protected Long solve1() {
-        for (int i = 0; i < input.size(); i += 7) {
-            monkeys.add(new Monkey(input.subList(i, i + 6)));
-            divisors.add(Integer.parseInt(input.get(i + 3).substring(21)));
-        }
-        for (int i = 0; i < 20; i++) {
-            monkeys.forEach(Monkey::throwItems);
-        }
-        return monkeys.stream()
-                .mapToLong(m -> m.inspects)
-                .sorted()
-                .skip(monkeys.size() - 2L)
-                .reduce(1, (i1, i2) -> i1*i2);
+        loadMonkeys();
+        return throwAndResult(20);
     }
 
     @Override
     public Long solve2() {
         part2 = true;
+        loadMonkeys();
+        supermodulo = 1;
+        for (Monkey monkey : monkeys) {
+            supermodulo = Util.lcm(supermodulo, monkey.modulo);
+        }
+        return throwAndResult(10_000);
+    }
+
+    private void loadMonkeys() {
         monkeys.clear();
         for (int i = 0; i < input.size(); i += 7) {
             monkeys.add(new Monkey(input.subList(i, i + 6)));
         }
-        for (int i = 0; i < 10_000; i++) {
-            Timer.tick(i);
+    }
+
+    private Long throwAndResult(int throwing) {
+        for (int i = 0; i < throwing; i++) {
             monkeys.forEach(Monkey::throwItems);
         }
         return monkeys.stream()
                 .mapToLong(m -> m.inspects)
                 .sorted()
                 .skip(monkeys.size() - 2L)
-                .reduce(1, (i1, i2) -> i1*i2);
+                .reduce(1, (i1, i2) -> i1 * i2);
     }
 
     private class Monkey {
-        long inspects = 0;
+        int inspects = 0;
+        List<Long> items = new ArrayList<>();
+        LongUnaryOperator operation;
         int modulo;
-        List<Item> items = new ArrayList<>();
-        boolean multiply;
-        int factor;
-        boolean square;
-        int monkeyTrue;
-        int monkeyFalse;
+        int trueMonkey;
+        int falseMonkey;
 
         Monkey(List<String> list) {
             Arrays.stream(list.get(1).substring(18).split(", "))
-                    .forEach(s -> items.add(new Item(Integer.parseInt(s))));
-            multiply = list.get(2).charAt(23) == '*';
-            if ("old".equals(list.get(2).substring(25))) {
-                square = true;
+                    .forEach(s -> items.add(Long.parseLong(s)));
+            String operand = list.get(2).substring(25);
+            if (list.get(2).charAt(23) == '*') {
+                if ("old".equals(operand)) {
+                    operation = o -> o * o;
+                } else {
+                    operation = o -> o * Integer.parseInt(operand);
+                }
             } else {
-                factor = Integer.parseInt(list.get(2).substring(25));
+                operation = o -> o + Integer.parseInt(operand);
             }
             modulo = Integer.parseInt(list.get(3).substring(21));
-            monkeyTrue = Integer.parseInt(list.get(4).substring(29));
-            monkeyFalse = Integer.parseInt(list.get(5).substring(30));
+            trueMonkey = Integer.parseInt(list.get(4).substring(29));
+            falseMonkey = Integer.parseInt(list.get(5).substring(30));
         }
 
         private void throwItems() {
             inspects += items.size();
-            for (Item item : items) {
-                if (square) {
-                    item.square();
-                } else if (multiply) {
-                    item.multiply(factor);
-                } else {
-                    item.add(factor);
-                }
+            for (long item : items) {
+                item = operation.applyAsLong(item);
                 if (!part2) {
-                    item.value = item.value / 3;
+                    item /= 3;
                 }
-                if (item.modulo(modulo) == 0) {
-                    monkeys.get(monkeyTrue).items.add(item);
-                } else {
-                    monkeys.get(monkeyFalse).items.add(item);
-                }
+                int targetMonkey = item % modulo == 0 ? trueMonkey : falseMonkey;
+                monkeys.get(targetMonkey).items.add(item % supermodulo);
             }
             items.clear();
-        }
-    }
-
-    private class Item {
-        int value;
-        Map<Integer, Integer> moduloMap = new HashMap<>();
-
-        Item(int value) {
-            this.value = value;
-            for (int divisor : divisors) {
-                moduloMap.put(divisor, value % divisor);
-            }
-        }
-
-        void square() {
-            value *= value;
-            for (Map.Entry<Integer, Integer> entry : moduloMap.entrySet()) {
-                entry.setValue((entry.getValue() * entry.getValue()) % entry.getKey());
-            }
-        }
-
-        void multiply(int factor) {
-            this.value *= factor;
-            for (Map.Entry<Integer, Integer> entry : moduloMap.entrySet()) {
-                entry.setValue((entry.getValue() * factor) % entry.getKey());
-            }
-        }
-
-        void add(int adder) {
-            this.value += adder;
-            for (Map.Entry<Integer, Integer> entry : moduloMap.entrySet()) {
-                entry.setValue((entry.getValue() + adder) % entry.getKey());
-            }
-        }
-
-        int modulo(int divisor) {
-            if (part2) {
-                return moduloMap.get(divisor);
-            } else {
-                return this.value % divisor;
-            }
         }
     }
 }
