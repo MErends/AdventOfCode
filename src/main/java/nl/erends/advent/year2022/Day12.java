46e46265273fecd52c3047f2131d2f6def391a2e
MErends <mark.erends@gmail.com>
2022-12-12 21:04:38 +0100

    2022 Day 12

--- src/main/java/nl/erends/advent/year2022/Day12.java
+++ src/main/java/nl/erends/advent/year2022/Day12.java
@@ -2,9 +2,9 @@ package nl.erends.advent.year2022;
 
 import nl.erends.advent.util.AbstractProblem;
 import nl.erends.advent.util.Direction;
+import nl.erends.advent.util.Point;
 import nl.erends.advent.util.Util;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -15,8 +15,12 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
- * --- Day 12
- * <p>
+ * --- Day 12: Hill Climbing Algorithm ---
+ * <p>You try contacting the Elves using your handheld device, but the river
+ * you're following must be too low to get a decent signal. You ask the device
+ * for a heightmap of the surrounding area (your puzzle input). What is the
+ * fewest steps required to move from your current position to the location that
+ * should get the best signal?
  * <p><a href="https://adventofcode.com/2022/day/12">2022 Day 12</a>
  */
 public class Day12 extends AbstractProblem<List<String>, Integer> {
@@ -30,7 +34,7 @@ public class Day12 extends AbstractProblem<List<String>, Integer> {
     @Override
     protected Integer solve1() {
         maze = new char[input.size()][];
-        Point start = new Point();
+        Point start = null;
         for (int y = 0; y < input.size(); y++) {
             maze[y] = input.get(y).toCharArray();
         }
@@ -45,12 +49,12 @@ public class Day12 extends AbstractProblem<List<String>, Integer> {
                 }
             }
         }
-        return solveMaze(start, p -> p.equals(end[0]), (p1, p2) -> maze[p1.y][p1.x] + 1 >= maze[p2.y][p2.x]);
+        return solveMaze(start, p -> p.equals(end[0]), (p1, p2) -> maze[p1.y()][p1.x()] + 1 >= maze[p2.y()][p2.x()]);
     }
 
     @Override
     public Integer solve2() {
-        return solveMaze(end[0], p -> maze[p.y][p.x] == 'a', (p1, p2) -> maze[p1.y][p1.x] - 1 <= maze[p2.y][p2.x]);
+        return solveMaze(end[0], p -> maze[p.y()][p.x()] == 'a', (p1, p2) -> maze[p1.y()][p1.x()] - 1 <= maze[p2.y()][p2.x()]);
     }
 
     private int solveMaze(Point start, Predicate<Point> winCondition, BiPredicate<Point, Point> stepCondition) {
@@ -61,20 +65,20 @@ public class Day12 extends AbstractProblem<List<String>, Integer> {
         int currentDistance = 0;
         List<Point> currentPoints = new ArrayList<>();
         currentPoints.add(start);
-        distances[start.y][start.x] = currentDistance;
+        distances[start.y()][start.x()] = currentDistance;
         while (!currentPoints.isEmpty()) {
             Set<Point> nextPoints = new HashSet<>();
             for (Point currentPoint : currentPoints) {
                 List<Point> neighbors = Stream.of(Direction.values())
-                        .map(d -> new Point(currentPoint.x + d.dx(), currentPoint.y + d.dy()))
-                        .filter(p -> p.x >= 0 && p.y >= 0 && p.y < maze.length && p.x < maze[p.y].length)
+                        .map(d -> new Point(currentPoint.x() + d.dx(), currentPoint.y() + d.dy()))
+                        .filter(p -> p.x() >= 0 && p.y() >= 0 && p.y() < maze.length && p.x() < maze[p.y()].length)
                         .toList();
                 for (Point neighbor : neighbors) {
-                    if (stepCondition.test(currentPoint, neighbor) && distances[neighbor.y][neighbor.x] > currentDistance) {
+                    if (stepCondition.test(currentPoint, neighbor) && distances[neighbor.y()][neighbor.x()] > currentDistance) {
                         if (winCondition.test(neighbor)) {
                             return currentDistance + 1;
                         }
-                        distances[neighbor.y][neighbor.x] = currentDistance + 1;
+                        distances[neighbor.y()][neighbor.x()] = currentDistance + 1;
                         nextPoints.add(neighbor);
                     }
                 }
