fbafd68c3e37840764d1368c4f0eb01e1b8161ad
MErends <mark.erends@gmail.com>
2022-12-14 10:42:30 +0100

    2022 Day 14

---
+++ src/test/java/nl/erends/advent/year2022/Day14Test.java
@@ -0,0 +1,18 @@
+package nl.erends.advent.year2022;
+
+import nl.erends.advent.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class Day14Test {
+
+    @Test
+    void day14Test() {
+        Day14 problem = new Day14();
+        problem.setInput(Util.readInput(2022, 14, 1));
+        assertThat(problem.solve1(), is (24));
+        assertThat(problem.solve2(), is (93));
+    }
+}
