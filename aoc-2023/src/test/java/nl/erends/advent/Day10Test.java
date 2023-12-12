7749f030ec27dcc067cb86c0ffd24007a5a8f922
MErends <mark@erends.nl>
2023-12-12 19:10:44 +0100

    2023 Day 10, Parameterized some tests

--- aoc-2023/src/test/java/nl/erends/advent/Day10Test.java
+++ aoc-2023/src/test/java/nl/erends/advent/Day10Test.java
@@ -1,45 +1,34 @@
 package nl.erends.advent;
 
 import nl.erends.advent.util.Util;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class Day10Test {
 
-    @Test
-    void day10Test1() {
+    @ParameterizedTest
+    @CsvSource({
+            "1, 4",
+            "2, 8",
+    })
+    void day10Test1(int testcase, int result) {
         Day10 problem = new Day10();
-        problem.setInput(Util.readInput(2023, 10, 1));
-        assertThat(problem.solve1(), is(4));
+        problem.setInput(Util.readInput(2023, 10, testcase));
+        assertThat(problem.solve1(), is(result));
     }
 
-    @Test
-    void day10Test2() {
+    @ParameterizedTest
+    @CsvSource({
+            "3, 4",
+            "4, 8",
+            "5, 10"
+    })
+    void day10Test3(int testcase, int result) {
         Day10 problem = new Day10();
-        problem.setInput(Util.readInput(2023, 10, 2));
-        assertThat(problem.solve1(), is(8));
-    }
-
-    @Test
-    void day10Test3() {
-        Day10 problem = new Day10();
-        problem.setInput(Util.readInput(2023, 10, 3));
-        assertThat(problem.solve2(), is(4));
-    }
-
-    @Test
-    void day10Test4() {
-        Day10 problem = new Day10();
-        problem.setInput(Util.readInput(2023, 10, 4));
-        assertThat(problem.solve2(), is(8));
-    }
-
-    @Test
-    void day10Test5() {
-        Day10 problem = new Day10();
-        problem.setInput(Util.readInput(2023, 10, 5));
-        assertThat(problem.solve2(), is(10));
+        problem.setInput(Util.readInput(2023, 10, testcase));
+        assertThat(problem.solve2(), is(result));
     }
 }
