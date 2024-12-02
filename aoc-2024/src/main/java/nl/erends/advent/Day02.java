e00c444c0afe40210195c638efc90c58834d89dd
MErends <mark@erends.nl>
2024-12-02 12:33:25 +0100

    2024 Day 2

---
+++ aoc-2024/src/main/java/nl/erends/advent/Day02.java
@@ -0,0 +1,86 @@
+package nl.erends.advent;
+
+import nl.erends.advent.util.AbstractProblem;
+import nl.erends.advent.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <h1>--- Day 2: Red-Nosed Reports ---</h1>
+ * <p>Your puzzle input consists of many reports, one report per line. Each
+ * report is a list of numbers called levels. Analyze the unusual data from the
+ * engineers. How many reports are safe?</p>
+ * <p><a href="https://adventofcode.com/2024/day/2">2024 Day 2</a></p>
+ */
+public class Day02 extends AbstractProblem<List<String>, Integer> {
+
+    public static void main(String[] args) {
+        new Day02().setAndSolve(Util.readInput(2024, 2));
+    }
+
+    @Override
+    protected Integer solve1() {
+        return (int) input.stream()
+                .map(Report::new).filter(Report::isSafe).count();
+    }
+
+    @Override
+    public Integer solve2() {
+        return (int) input.stream()
+                .map(Report::new).filter(Report::maybeSafe).count();
+    }
+
+    private static class Report {
+
+        private List<Integer> levels;
+
+        Report() {}
+
+        Report(String line) {
+            levels = Arrays.stream(line.split(" "))
+                    .map(Integer::parseInt).toList();
+        }
+
+        boolean isSafe() {
+            return strictlyIncreases() || strictlyDecreases();
+        }
+
+        boolean strictlyIncreases() {
+            for (int index = 0; index < levels.size() - 1; index++) {
+                int diff = levels.get(index) - levels.get(index + 1);
+                if (diff <= 0 || diff > 3) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean strictlyDecreases() {
+            for (int index = 0; index < levels.size() - 1; index++) {
+                int diff = levels.get(index) - levels.get(index + 1);
+                if (diff >= 0 || diff < -3) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean maybeSafe() {
+            if (isSafe()) {
+                return true;
+            }
+            for (int index = 0; index < levels.size(); index++) {
+                List<Integer> cloneList = new ArrayList<>(levels);
+                cloneList.remove(index);
+                Report cloneReport = new Report();
+                cloneReport.levels = cloneList;
+                if (cloneReport.isSafe()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ aoc-2024/src/test/java/nl/erends/advent/Day02Test.java
@@ -0,0 +1,18 @@
+package nl.erends.advent;
+
+import nl.erends.advent.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class Day02Test {
+
+    @Test
+    void day02Test1() {
+        Day02 problem = new Day02();
+        problem.setInput(Util.readInput(2024, 2, 1));
+        assertThat(problem.solve1(), is (2));
+        assertThat(problem.solve2(), is (4));
+    }
+}
