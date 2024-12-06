72ce159176176a0260ae23a9fc2e6cb9747b784e
MErends <mark@erends.nl>
2024-12-06 21:03:00 +0100

    2024 Day 6

---
+++ aoc-2024/src/main/java/nl/erends/advent/Day06.java
@@ -0,0 +1,119 @@
+package nl.erends.advent;
+
+import nl.erends.advent.util.AbstractProblem;
+import nl.erends.advent.util.Coord;
+import nl.erends.advent.util.Direction;
+import nl.erends.advent.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static nl.erends.advent.util.Direction.UP;
+
+/**
+ * <h1>--- Day 6: Guard Gallivant ---</h1>
+ * <p>A single guard is patrolling this part of the lab. Lab guards in 1518
+ * follow a very strict patrol protocol which involves repeatedly following
+ * these steps. If there is something directly in front of you, turn right 90
+ * degrees. Otherwise, take a step forward. Predict the path of the guard. How
+ * many distinct positions will the guard visit before leaving the mapped area?
+ * </p>
+ * <p><a href="https://adventofcode.com/2024/day/6">2024 Day 6</a></p>
+ */
+public class Day06 extends AbstractProblem<List<String>, Integer> {
+
+    public static void main(String[] args) {
+        new Day06().setAndSolve(Util.readInput(2024, 6));
+    }
+
+    @Override
+    protected Integer solve1() {
+        char[][] grid = readInput();
+        Coord guard = getStart(grid);
+        Direction d = UP;
+        boolean busy = true;
+        while (busy) {
+            grid[guard.y][guard.x] = 'X';
+            switch (getSafeGrid(grid, guard.x + d.dx(), guard.y + d.dy())) {
+                case '\0' -> busy = false;
+                case '#' -> d = d.turnRight();
+                default -> guard.addDirection(d);
+            }
+        }
+        int score = 0;
+        for (char[] line : grid) {
+            for (char c : line) {
+                if (c == 'X') {
+                    score++;
+                }
+            }
+        }
+        return score;
+    }
+
+    @Override
+    public Integer solve2() {
+        int obstructions = 0;
+        char[][] grid = readInput();
+        Coord guardStart = getStart(grid);
+        for (Coord wall : getPossibleWalls(grid)) {
+            grid = readInput();
+            grid[wall.y][wall.x] = '#';
+            Coord guard = new Coord(guardStart);
+            Direction d = UP;
+            int[][] passes = new int[grid.length][grid[0].length];
+            boolean busy = true;
+            while (busy) {
+                if (passes[guard.y][guard.x] == 4) { // Went by in all directions?!
+                    obstructions++;
+                    break;
+                }
+                passes[guard.y][guard.x]++;
+                switch (getSafeGrid(grid, guard.x + d.dx(), guard.y + d.dy())) {
+                    case '\0' -> busy = false;
+                    case '#' -> d = d.turnRight();
+                    default -> guard.addDirection(d);
+                }
+            }
+        }
+        return obstructions;
+    }
+
+    private char getSafeGrid(char[][] grid, int x, int y) {
+        if (y < 0 || y >= grid.length || x < 0 || x >= grid[y].length) {
+            return '\0';
+        }
+        return grid[y][x];
+    }
+
+    private char[][] readInput() {
+        char[][] grid = new char[input.size()][];
+        for (int y = 0; y < input.size(); y++) {
+            grid[y] = input.get(y).toCharArray();
+        }
+        return grid;
+    }
+
+    private Coord getStart(char[][] grid) {
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] == '^'){
+                    return new Coord(x, y);
+                }
+            }
+        }
+        throw new IllegalStateException("No start found!");
+    }
+
+    private List<Coord> getPossibleWalls(char[][] grid) {
+        List<Coord> possibleWalls = new ArrayList<>();
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] == '.') {
+                    possibleWalls.add(new Coord(x, y));
+                }
+            }
+        }
+        return possibleWalls;
+    }
+}
