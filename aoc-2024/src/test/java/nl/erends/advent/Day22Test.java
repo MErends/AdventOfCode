55ac5a0801f5fdb1ff88aeef2bfad0af8dbabb14
MErends <mark@erends.nl>
2024-12-29 12:07:07 +0100

    2024 Day 22

---
+++ aoc-2024/src/test/java/nl/erends/advent/Day22Test.java
@@ -0,0 +1,24 @@
+package nl.erends.advent;
+
+import nl.erends.advent.util.Util;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class Day22Test {
+
+    @Test
+    void day22Test1() {
+        Day22 problem = new Day22();
+        problem.setInput(Util.readIntegers(2024, 22, 1));
+        assertThat(problem.solve1(), is (37327623L));
+    }
+
+    @Test
+    void day22Test2() {
+        Day22 problem = new Day22();
+        problem.setInput(Util.readIntegers(2024, 22, 2));
+        assertThat(problem.solve2(), is (23L));
+    }
+}
