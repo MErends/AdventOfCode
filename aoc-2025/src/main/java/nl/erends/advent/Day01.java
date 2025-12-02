6cf6fc0afc1562181a484a6819e082c20a07b810
MErends <mark@erends.nl>
2025-12-02 19:27:23 +0100

    2025 Day 1

--- aoc-2025/src/main/java/nl/erends/advent/Day01.java
+++ aoc-2025/src/main/java/nl/erends/advent/Day01.java
@@ -5,6 +5,15 @@ import nl.erends.advent.util.Util;
 
 import java.util.List;
 
+/**
+ * <h1>--- Day 1: Secret Entrance ---</h1>
+ * <p>You arrive at the secret entrance to the North Pole base ready to start
+ * decorating. Unfortunately, the password seems to have been changed, so you
+ * can't get in. Due to new security protocols, the password is locked in the
+ * safe. The actual password is the number of times the dial is left pointing at
+ * 0 after any rotation in the sequence.</p>
+ * <p><a href="https://adventofcode.com/2025/day/1">2025 Day 1</a></p>
+ */
 public class Day01 extends AbstractProblem<List<String>, Integer> {
 
     void main() {
